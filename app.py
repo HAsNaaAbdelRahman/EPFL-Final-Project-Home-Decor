abf4d80d044f19077075383e4f0ac20ff277dfd2
Hasnaa <a02040456@gmail.com>
2025-08-11 11:59:57 +0300

    add Routes

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask , redirect , render_template , request
-
+from routes.home_routes import home_route
 app = Flask(__name__)
 
 
+home_route(app)
 
 if __name__ == "__main__":
     app.run(debug="True")
