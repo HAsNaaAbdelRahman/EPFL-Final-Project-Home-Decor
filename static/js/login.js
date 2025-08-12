fc257b166255140134bab04df39e47e9becf9732
Hasnaa <a02040456@gmail.com>
2025-08-12 17:51:54 +0300

    add Js Files

---
+++ static/js/login.js
@@ -0,0 +1,37 @@
+document.querySelector('form').addEventListener("submit" , function(e)
+{
+    e.preventDefault();
+})
+
+const email = document.querySelector('input[name = "email"]')
+const password = document.querySelector('input[name = "password"]')
+
+fetch('/login' , {
+ method: 'POST',
+ body: new URLSearchParams({email , password} ),
+ headers: {
+     'Content-Type': 'application/x-www-form-urlencoded',
+ },
+})
+.then(response => response.json())
+.then(data => {
+    if(data.id) {
+        localStorage.setItem('user' , JSON.stringify(data));
+        window.location.href = '/home';
+    }else {
+        alert(data.error);
+    }
+})
+.catch(() => {
+  alert('An error occurred while logging in.');  
+});
+
+function checkUserSession() {
+  const user = localStorage.getItem('user');
+  if (user) {
+    const userData = JSON.parse(user);
+  } else {
+    
+  }
+}
+window.onload = checkUserSession;
