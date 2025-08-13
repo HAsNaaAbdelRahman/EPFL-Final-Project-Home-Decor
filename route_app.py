cbed33cf3acc27151386e9cddddd8a5897b68352
Hasnaa <a02040456@gmail.com>
2025-08-13 13:02:02 +0300

    enhance dependancies

--- route_app.py
+++ route_app.py
@@ -1,6 +1,6 @@
 from flask import Flask , redirect , render_template
 from routes.home import home_page
-from routes.account_routes import account
+from routes.account_routes import account_routes
 from routes.password import password_op
 from flask_session import Session
 
@@ -13,8 +13,9 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 
+
 home_page(app)
-account(app)
+account_routes(app)
 password_op(app)
 
 if __name__ == "__main__":
